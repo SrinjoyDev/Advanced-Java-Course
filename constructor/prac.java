3f4edf8dd5135ade23255b6d43ad95b3bc9554a8
Srinjoy Dev <srinjoydev1104@gmail.com>
2025-11-10 09:26:45 +0000

    done: constructors

--- constructor/prac.java
+++ constructor/prac.java
@@ -7,8 +7,22 @@ class Human {
   //constructor>>
   //constructor has the same name as the class name
   //and also constructors doesnt have any return types.
-  public Human() {
+  public Human() { //default constrictor
     System.out.println("in constructor!!");
+
+    //here by defailt we can assign some values.
+    //so when the object is defined and the constructor laods.
+    //we will have some values.
+    age = 21;
+    name = "Srinjoy another";
+  }
+
+  public Human(String name , int age){ //parameterised constructor.
+    //we make a constructor accepting arguments so that , 
+    //on object is defined for that class ,some values of that can
+    //be created.
+    this.name = name;
+    this.age = age;
   }
 
   //getters
@@ -43,13 +57,13 @@ public class prac {
     //then put values for that.
     //normal method ::
 
-    Human object = new Human();
+    //Human object = new Human();
     //setage and set name
-    object.setAge(11);
-    object.setName("Srinjoy");
+    //object.setAge(11);
+    //object.setName("Srinjoy");
 
-    System.out.println(object.getName());
-    System.out.println(object.getAge());
+    //System.out.println(object.getName());
+    //System.out.println(object.getAge());
 
     //above was the normal method which we can do .
     //but provlem with that is that we are creating an 
@@ -59,5 +73,15 @@ public class prac {
     //but to have some vairables while defineing the 
     //object , we need to do this :
     //Thats why constructors are there.
+    
+    Human obj = new Human();
+    System.out.println("name : " + obj.getName());
+    System.out.println("age : " + obj.getAge());
+
+    //lets say we dont want the predifined values of the class
+    //we want to have our own values when we create the object.
+    Human obj1 = new Human("shakir", 3);
+    System.out.println("new object name" + obj1.getName());
+    System.out.println("new object age : " + obj1.getAge());
   }
 }
