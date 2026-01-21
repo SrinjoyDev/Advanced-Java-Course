a0e4909495995eea5507e535a0f6eb864dd4abe9
Srinjoy Dev <srinjoydev1104@gmail.com>
2026-01-21 15:12:09 +0000

    thread priority and theead timing and sleeping

--- threads/Main.java
+++ threads/Main.java
@@ -11,6 +11,15 @@ class Hi extends Thread implements ThreadImplementation {
     public void run() { //classses that extends a thread , should have a run method that runs when a thread is started
         for(int i = 0 ; i < 100 ; i ++){
             System.out.println("hi");
+            //tell thge thread to sleep for 10ms , so that we get sequent8ial hi , hello printing.
+            //Note : somewhere u will see u will get 2 hellos at the same time , or 2 hi's at the same time.
+            ///that is becauase the threads are not in sequience , that is how it works . we can optimise it more , by sleeping the hread between exeuction of those in 2 milliseconds , see the main method now .
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            
         }
     }
 }
@@ -19,6 +28,11 @@ class Hello extends Thread implements ThreadImplementation {
     public void run() {
         for(int i = 0 ; i < 100 ; i ++){
             System.out.println("hello");
+            try {
+                Thread.sleep(10);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
@@ -41,7 +55,23 @@ public class Main {
         Hi obj1 = new Hi();
         Hello obj2 = new Hello();
 
+        //getting a priority of a thread.
+        System.out.println(obj1.getPriority());
+        //set priprity
+        obj1.setPriority(Thread.MAX_PRIORITY); //suggesting our os scheduler that this thread should get the highest priority.
+        obj2.setPriority(Thread.MIN_PRIORITY); //same but opposite analogy here.
+
+        //NOTE: priority dosent mean that , the thread with highest priority will run first.
+        //it will only prioritisise when the threads are reaching a stage where they are reachingh to a point at the same time
+        //then the scheduler will decide who to give execution first based on the priority set.
+        //also scheduler also decides execution based on the execution time  of the thread , lowest executing timed thread gets more priority first.
+
         obj1.start(); //start the thread , will execure the method
+        try {
+            Thread.sleep(2);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         obj2.start(); //start the thread
 
     }
