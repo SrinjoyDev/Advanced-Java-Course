95f399025b46b3ab6413791e8eec880bd1d7937a
Srinjoy Dev <srinjoydev1104@gmail.com>
2026-01-22 13:43:26 +0000

    multithreading question

--- multithreadingAss/Main.java
+++ multithreadingAss/Main.java
@@ -1,5 +1,30 @@
 package multithreadingAss;
 
+class PrinterTask implements Runnable {
+    public void run() {
+        String threadName = Thread.currentThread().getName();
+        for(int i = 1 ; i <= 5 ; i ++){
+            System.out.println( threadName + " Running task" +  i);
+        }
+    }
+}
+
 public class Main {
-    
+    public static void main(String[] args)  {
+        PrinterTask pt = new PrinterTask();
+
+        Thread worker1 = new Thread(pt , "worker-1");
+        Thread worker2 = new Thread(pt , "worker-2");
+
+        worker1.start();
+        worker2.start();
+
+        try {
+            worker1.join();
+            worker2.join();    
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
 }
